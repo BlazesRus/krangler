45ee672f2443e0390b03c6f94c0b51bb1733d734
BlazesRus (James) <BlazesRus@yahoo.com>
2023-11-05 18:10:17 -0500

    Adding unfinished parsing code to treat lua file as node groups etc

--- krangler.py
+++ krangler.py
@@ -1,3 +1,4 @@
+from tkinter import INSIDE
 import pandas as pd
 import numpy as np
 import glob
@@ -42,17 +43,66 @@ def replace_all_nodes_wrapper(inputDirectory, outputDirectory):
 
 def replace_all_nodes(modified_tree, original_tree, outputDirectory, basedir='./data/'):
     all_jsons = glob.glob(basedir+'*.json')
+    #previousNodeGroup = 'TopOfTree'
     if len(all_jsons) < 1:
+    #{
         print('No JSON files found in data directory...Converting all nodes into nothing instead \n')
         #Replace with code that doesn't use json file later
-        all_node_data = pd.read_json('all_nodes_nothingness.json', typ='series')
-        node_df = pd.DataFrame(all_node_data).reset_index().rename(columns = {'index':'original', 0:'new'})
-        node_df = node_df.drop_duplicates()
-        nothingness_dupes = node_df[node_df['original'].duplicated(keep=False)]
-        node_df = node_df.drop(nothingness_dupes[nothingness_dupes['new']==-1].index)
-        for line in range(len(node_df)):
-            replace_node_with_nothing(modified_tree, original_tree, int(node_df.iloc[line]['original']))
+        # all_node_data = pd.read_json('all_nodes_nothingness.json', typ='series')
+        # node_df = pd.DataFrame(all_node_data).reset_index().rename(columns = {'index':'original', 0:'new'})
+        # node_df = node_df.drop_duplicates()
+        # nothingness_dupes = node_df[node_df['original'].duplicated(keep=False)]
+        # node_df = node_df.drop(nothingness_dupes[nothingness_dupes['new']==-1].index)
+        # for line in range(len(node_df)):
+        #     replace_node_with_nothing(modified_tree, original_tree, int(node_df.iloc[line]['original']))
+
+        #Parsing lines into nodes instead
+        inside_nodeLevel = False
+        topLevel_luaNodeLineNumber = -1
+        topLevel_luaNodeName = ''
+        LineChar = '';
+        #InsideLuaComment = false;
+        lineNumber = -1;
+        
+        topLevelStorage = { "RootStart" : "" }
+
+        for line in original_tree:#{
+            ++lineNumber;
+            if(topLevel_luaNodeLineNumber==-1):#Add to root level before actual node info starts
+            #{
+                if(line.contains('[')):
+                #{
+                    topLevel_luaNodeLineNumber = lineNumber;
+                    topLevelStorage[topLevel_luaNodeName] = line;
+                    if(line.contains('{')):
+                        inside_nodeLevel = True
+                #}
+                else:
+                #{
+                    topLevelStorage["RootStart"] += line;
+                #}
+            #}
+            elif inside_nodeLevel == False:
+            #{
+                if(line.contains('[')):
+                #{
+                    topLevel_luaNodeLineNumber = lineNumber;
+                #}
+                else:
+                #{
+                    if "RootEnd" in topLevelStorage:
+                        topLevelStorage["RootEnd"] += line;
+                    else:
+                        topLevelStorage["RootEnd"] = line;
+                #}
+            #}
+            else:
+            #{
+            #}
+        #}
+    #}
     else:
+    #{
         #Files need to follow stringify 4 space format with 2 spaces on bracket or similar format or will fail to read json files (minify format will most likely fail)
         all_node_data = pd.concat([pd.read_json(json_file, typ='series', dtype='dict', encoding_errors='ignore') for json_file in all_jsons], verify_integrity=True)
         #print('node_df stage starting \n')
@@ -70,13 +120,15 @@ def replace_all_nodes(modified_tree, original_tree, outputDirectory, basedir='./
                 print('mismatch original node: '+str(node_df.iloc[node_id]['original'])) #+', new: '+str(node_df.iloc[node_id]['new']))
 
         for line in range(len(node_df)):
+            #Sending information of node group passed to make sure don't replace any important nodes
             replace_node(modified_tree, original_tree,
-                    int(node_df.iloc[line]['original']), int(node_df.iloc[line]['new']))
-
+            int(node_df.iloc[line]['original']), int(node_df.iloc[line]['new']))
+    #}
     save_tree(modified_tree, outputDirectory)
 
 #Use breakpoints at error message prints in Visual Studio to find bugs in json file
 
+# Maybe returning value of current node group if non-integer node
 def replace_node_with_nothing(modified_tree, original_tree, node_id):
     if type(node_id) == str:
         node_found, node_id = get_node_id_by_name(original_tree, node_id)
@@ -112,73 +164,105 @@ def replace_node_with_nothing(modified_tree, original_tree, node_id):
     return modified_tree
 
 def replace_node(modified_tree, original_tree, node_id, replace_id):
-    is_MismatchedType = False
-    if type(node_id) == str:#checking if string formated node id is found?
-        node_found, node_id = get_node_id_by_name(original_tree, node_id)
-        if not(node_found):
-            print('Original string node with id:'+ str(node_id) +' not found. \n')
-            return modified_tree
-    else:
-        node_found, node_id = get_node_id_by_name(original_tree, node_id)
-        if not(node_found):
-            print('Original non-string node with id:'+ str(node_id) +' not found. \n')
-            return modified_tree
-    replace_found = False
-    replacement_found_by_name = False
-    node_start = 0
-    node_end = 0
-    if replace_id==-1:
-        replace_found = True
-    else:
-        if type(replace_id) == str:
-            #checking if replacement node exists by name on original tree
-            replace_found, replace_id = get_node_id_by_name(original_tree, replace_id)
-            if replace_found == False:
-                #if replacement node doesn't exist by name, check if exists by id on original tree
-                node_found, node_start, node_end = get_node_by_id(original_tree, node_id)
-                if replace_id > 0 and replace_found:
-                    replace_found, replace_start, replace_end = get_node_by_id(original_tree, replace_id)
-                    if replace_found == False:
-                        print('Replacement '+str(replace_id)+' for node '+str(node_id)+' not found \n')
-            
-    is_ascendancy = False
-    is_mastery = False
-    for line_idx in range(node_end-node_start):
-        if 'ascendancyName' in original_tree[node_start]:
-            if replace_found:
-                if 'ascendancyName' in modified_tree[node_start]:
-                    ascendancy_line = modified_tree[node_start]
-                else:
-                    print('Replacement node does not match node type')
-                    is_MismatchedType = True
-            else:
-                replace_lines = [
-'            [\"name\"] = \"Ascendancy Replacement Error for node '+str(node_id)+' with invalid id '+str(replace_id)+'\",\n',
-'            [\"icon\"] = \"Art/2DArt/SkillIcons/passives/MasteryBlank.png\",\n',
-'            [\"ascendancyName\"] = \"None\",\n',
-'            [\"stats\"] = {},\n']
-                replace_start = 0
-                replace_end = 4
-            is_ascendancy = True
-        modified_tree.pop(node_start)
-    if replace_id > 0:
-        replace_lines = original_tree[replace_start:replace_end]
-    elif is_ascendancy:
-        replace_lines = NOTHINGNESS_ASCENDANCY
-        replace_start = 0
-        replace_end = 4
-    else:
-        replace_lines = NOTHINGNESS
-        replace_start = 0
-        replace_end = 3
-    for replace_idx, line_idx in enumerate(range(node_start, node_start+replace_end-replace_start)):
-        if 'ascendancyName' in replace_lines[replace_idx]:
-            try:
-                modified_tree.insert(line_idx, ascendancy_line)
-            except:
-                print('error: node '+str(node_id)+'; replace: '+str(replace_id))
+    Does_TargetNodeHaveID = False
+    node_index = -1#avoid replacing parameter info
+    node_start = -1
+    node_end = -1
+    if isinstance(node_id, int):#{
+        #Check to make sure both replacement and node_id have same keytype
+        if isinstance(replace_id, int):
+            Does_TargetNodeHaveID = True
         else:
-            modified_tree.insert(line_idx, replace_lines[replace_idx])
+            return
+        node_found, node_start, node_end = get_node_by_id(original_tree, node_id)
+        if not(node_found):
+            print('Original node with id:'+ str(node_id) +' not found. \n')
+            return
+        #else:#maybe check if is mastery node and either skip or change the node
+    #}
+    else:
+    #{
+        #Check to make sure both replacement and node_id have same keytype
+        if isinstance(replace_id, int):
+            return
+            
+        node_found, node_index = get_node_id_by_name(original_tree, node_id)
+        if not(node_found):
+            print('Original string node with name:'+ str(node_id) +' not found. \n')
+            return
+        else:
+            print('Original string node with name:'+ str(node_id) +' found. \n')
+    #}
+    #is_MismatchedType = False
+    is_ascendancy = False
+    #is_mastery = False
+
+    #Only replace non-parent group nodes
+    if Does_TargetNodeHaveID:
+    #{
+        #if(p)
+
+        #checking subnode information
+        for line_idx in range(node_end-node_start):#{
+            if 'ascendancyName' in original_tree[node_start]:
+                if 'ascendancyName' in original_tree[replace_start]:
+                    ascendancy_line = original_tree[node_start]
+                else:
+                    print('Mismatched ascendancy node named '+node_id+' attempted to replace with node'+str(replace_id)+'. \n')
+                    return
+            elif 'isMastery' in original_tree[node_start]:
+                if 'isMastery' not in original_tree[node_start]:
+                    print('Mismatched Mastery node named '+node_id+' attempted to replace with node'+str(replace_id)+'. \n')
+                    return
+            #checking to make sure not jewel socket
+            elif 'isJewelSocket' in original_tree[node_start]:
+                return modified_tree
+            #Notibles can only be replaced with notibles
+            elif 'isNotable' in original_tree[node_start]:
+                if 'isNotable' not in original_tree[replace_start]:
+                    print('Mismatched Notable node named '+node_id+' attempted to replace with node'+str(replace_id)+'. \n')
+                    return
+            #Remving last element from list
+            modified_tree.pop(node_start)
+        #}
+
+        if replace_id > 0:
+        #{
+            replace_lines = original_tree[replace_start:replace_end]
+        #}
+        #Replacing every numbered Node with nothingness
+        elif is_ascendancy:
+            replace_lines = NOTHINGNESS_ASCENDANCY
+            replace_start = 0
+            replace_end = 4
+        else:
+            replace_lines = NOTHINGNESS
+            replace_start = 0
+            replace_end = 3
+
+        for replace_idx, line_idx in enumerate(range(node_start, node_start+replace_end-replace_start)):#{
+            if 'ascendancyName' in replace_lines[replace_idx]:
+                try:
+                    modified_tree.insert(line_idx, ascendancy_line)
+                except:
+                    print('error: node '+str(node_id)+'; replace: '+str(replace_id))
+            else:
+                modified_tree.insert(line_idx, replace_lines[replace_idx])
+        #}
+    #}
+    else:
+        replace_found, replacement_index  = get_node_id_by_name(original_tree, replace_id)
+        if replace_found == False:
+            print('Replacement '+str(replace_id)+' for node named '+node_id+' not found. \n')
+            return modified_tree
+        if(replacement_index!=-1):
+        #{
+            print('Matching replaceid('+str(replacement_index)+') found for replacement node named '+replace_id)
+        #}
+        #if node_id=="classes":#inside class info node
+        #if node_id=="nodes":#inside node group storage
+
+
     return modified_tree
 
 def get_node_by_id(tree, node_id):
